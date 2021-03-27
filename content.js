d2e6c7c550273be5b6d1296a2de576ce43e29f1e
Mateusz <mateuszobrzut628@gmail.com>
2021-03-27 22:26:30 +0100

    update

--- content.js
+++ content.js
@@ -11,14 +11,62 @@ function createGuid() {
   return guid.toLowerCase();
 }
 
+// TODO: maybe move to main?
+// firebase configs and initialization
+const firebaseConfig = {
+  apiKey: "AIzaSyBFuAXYGtjU9-abDvGA5eDTAkPB-fAoYv4",
+  authDomain: "angularfirecourse-f1155.firebaseapp.com",
+  databaseURL: "https://angularfirecourse-f1155.firebaseio.com",
+  projectId: "angularfirecourse-f1155",
+  storageBucket: "angularfirecourse-f1155.appspot.com",
+  messagingSenderId: "97150647711",
+  appId: "1:97150647711:web:1fd80ec04cad620b7b91aa",
+  measurementId: "G-0FCXLZJ266"
+}
+firebase.default.initializeApp(firebaseConfig)
+
+
+const encode = string => string.replace(/\./g, '<').replace(/\//g, '>')
+const decode = string => string.replace(/</g, '.').replace(/>/g, '/')
+
+const pageURL = encode(`${location.hostname}${location.pathname}`)
+const pageRef = firebase.default.database().ref('/pages').child(pageURL)
+
+let a = {}
+pageRef.on('value', snap => {
+  const data = snap.val()
+  console.log(data)
+
+  for (let id in data) {
+    if(a[id]) continue
+    
+    const mark = create_mark_with_popup(id, RangeUtils.toRange(data[id].range))
+
+    a[id] = mark
+  }
+})
+
 /**
  * The 'C' in CRUD for the comment store.
  */
+
 function addComment(comment) {
   console.log("Inserting new comment to database");
   var id = createGuid();
   pageRef.child(id).set(comment)
   // firebase.database().ref(`/users/${comment.user}`).child(id).set(comment);
+}
+
+function addCommentToHiglhlight(highlightId) {
+  // get highlight by id
+  user = 'mati'; // TODO: change user 
+  text = document.getElementById(highlightId).innerText;
+  // const pageURL = encode(`${location.hostname}${location.pathname}`)
+  // firebase.default.database().ref(`/pages/${encodeURIComponent(window.location.href)}/${highlightId}`)
+  var updates = {}
+  updates[`/${highlightId}/comment`] = text
+  pageRef.child(highlightId).update(updates);
+  // add comment to highlight id
 }
 
 
@@ -82,36 +130,3 @@ function create_message_node(message_content){
   return message_node
 }
 
-// firebase configs and initialization
-const firebaseConfig = {
-  apiKey: "AIzaSyBFuAXYGtjU9-abDvGA5eDTAkPB-fAoYv4",
-  authDomain: "angularfirecourse-f1155.firebaseapp.com",
-  databaseURL: "https://angularfirecourse-f1155.firebaseio.com",
-  projectId: "angularfirecourse-f1155",
-  storageBucket: "angularfirecourse-f1155.appspot.com",
-  messagingSenderId: "97150647711",
-  appId: "1:97150647711:web:1fd80ec04cad620b7b91aa",
-  measurementId: "G-0FCXLZJ266"
-}
-firebase.default.initializeApp(firebaseConfig)
-
-
-const encode = string => string.replace(/\./g, '<').replace(/\//g, '>')
-const decode = string => string.replace(/</g, '.').replace(/>/g, '/')
-
-const pageURL = encode(`${location.hostname}${location.pathname}`)
-const pageRef = firebase.default.database().ref('/pages').child(pageURL)
-
-let a = {}
-pageRef.on('value', snap => {
-  const data = snap.val()
-  console.log(data)
-
-  for (let id in data) {
-    if(a[id]) continue
-    console.log(id)
-    const mark = create_mark_with_popup(RangeUtils.toRange(data[id].range), id)
-
-    a[id] = mark
-  }
-})
